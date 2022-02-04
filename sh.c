54001387063d762d30a796c5ddd688dc1b6b9d06
miguel partida <ma.gonzalezp33@gmail.com>
2022-02-03 19:11:54 -0600

    Added sh.c

---
+++ sh.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <string.h>
+
+char  cmd[80];
+
+void main(){
+	pid_t p;
+	char* argv[2] = {NULL,NULL};
+	printf(">");
+	while(scanf("%s",cmd),strcmp(cmd,"exit") && strcmp(cmd,"shutdown")){
+		p = fork();
+		argv[0] = cmd;
+		if(!p){
+			execvp(cmd, argv);
+		}
+		wait(NULL);
+		printf(">");
+	}
+}
